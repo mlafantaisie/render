3d7bdec272d99706da61007e2ac5f41f3054d052
Matthew <74075600+mlafantaisie@users.noreply.github.com>
2025-03-13 12:45:17 -0400

    Update urls.py

--- pythonbasics/urls.py
+++ pythonbasics/urls.py
@@ -2,10 +2,12 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 from .views import upload_data
+from .views import data_dashboard
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", views.home, name="home"),
     path("about/", views.about, name="about"),
     path("upload/", upload_data, name="upload_data"),
+    path("dashboard/", data_dashboard, name="dashboard"),
 ]
