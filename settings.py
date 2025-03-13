6bbd305e7e117594456217c64f2d3c40eea6ca45
Matthew <74075600+mlafantaisie@users.noreply.github.com>
2025-03-13 09:37:19 -0400

    Update settings.py

--- settings.py
+++ settings.py
@@ -2,3 +2,22 @@ import os
 import dj_database_url
 
 ALLOWED_HOSTS = ["https://python-basics.onrender.com", "localhost"]
+
+DATABASES = {
+  'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
+}
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+
+# Whitenoise for serving static files in production
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',  # Add this line
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
