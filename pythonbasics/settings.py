8a6caaca8916f2065afb37f3d1cc949ad9a769a8
Matthew <74075600+mlafantaisie@users.noreply.github.com>
2025-03-13 12:49:35 -0400

    Update settings.py

--- pythonbasics/settings.py
+++ pythonbasics/settings.py
@@ -18,6 +18,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'pythonbasics',
 ]
 
 # Whitenoise for serving static files in production
