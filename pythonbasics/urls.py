ccdf2fbc2f300f64d55d32ca706eba01cf9c2c6f
Matthew <74075600+mlafantaisie@users.noreply.github.com>
2025-03-13 12:41:44 -0400

    Update urls.py

--- pythonbasics/urls.py
+++ pythonbasics/urls.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
 from django.urls import path
 from . import views
+from .views import upload_data
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path("", views.home, name="home"),
     path("about/", views.about, name="about"),
+    path("upload/", upload_data, name="upload_data"),
 ]
