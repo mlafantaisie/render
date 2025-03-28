b520b8a2e511286c68f5caee12043df242788622
Matthew <74075600+mlafantaisie@users.noreply.github.com>
2025-03-28 13:50:17 -0400

    Update forms.py

--- core/forms.py
+++ core/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 
 class UploadAccessDBForm(forms.Form):
+    site_code = forms.CharField(required=False, label='Site Code (optional)', max_length=50)
     accdb_file = forms.FileField(
         label='Access DB File (.accdb or .r5c)',
         help_text='Upload a Microsoft Access database file for parsing.'
