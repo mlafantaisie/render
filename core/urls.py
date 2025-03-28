99dbc04fde45b1d8247c0fd3d0230996474cafc2
Matthew <74075600+mlafantaisie@users.noreply.github.com>
2025-03-28 13:54:11 -0400

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
 from django.urls import path
 from . import views
-from .views import upload_data
-from .views import data_dashboard
+from .views import upload_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
