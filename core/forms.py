02bcd4a4db8bf222858b376f2f4a2a85d7aadc40
Matthew <74075600+mlafantaisie@users.noreply.github.com>
2025-03-28 12:14:23 -0400

    Update forms.py

--- core/forms.py
+++ core/forms.py
@@ -2,3 +2,7 @@ from django import forms
 
 class UploadFileForm(forms.Form):
     file = forms.FileField()
+
+class UploadAccessDBForm(forms.Form):
+    site_code = forms.CharField(label='Site Code', max_length=50)
+    accdb_file = forms.FileField(label='Access DB File (.accdb)')
