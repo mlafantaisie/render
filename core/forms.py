4412c6ec4b5e0ef202da5391f92a576182069d97
Matthew <74075600+mlafantaisie@users.noreply.github.com>
2025-03-31 15:18:11 -0400

    Update forms.py

--- core/forms.py
+++ core/forms.py
@@ -4,4 +4,7 @@ class UploadAccessDBForm(forms.Form):
     site_code = forms.CharField(label='Site Code', max_length=50, required=False)
     accdb_file = forms.FileField(label='Access DB File (.accdb)', required=False)
     offset = forms.IntegerField(label='Inspect Offset (Optional)', required=False, min_value=0)
+    chunk_size = forms.IntegerField(
+        label='Bytes to Read (Optional)', required=False, min_value=16, max_value=512
+    )
     
