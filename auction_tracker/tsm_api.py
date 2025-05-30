644a2838085a30b87def641fbeb7e66728b98e6a
Matthew <74075600+mlafantaisie@users.noreply.github.com>
2025-05-30 15:30:36 -0400

    Update tsm_api.py

--- auction_tracker/tsm_api.py
+++ auction_tracker/tsm_api.py
@@ -18,7 +18,7 @@ def get_access_token():
     return response.json()['access_token']
 
 def get_moon_guard_ah_id():
-    url = "https://realm-api.tradeskillmaster.com/region/1/realms?gameVersion=retail"
+    url = "https://realm-api.tradeskillmaster.com/realms"
     response = requests.get(url)
     response.raise_for_status()
 
