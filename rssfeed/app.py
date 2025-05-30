3d2d0953a50956305a9365ed3833901511a6a4d1
Matthew <74075600+mlafantaisie@users.noreply.github.com>
2025-05-30 10:53:59 -0400

    Update app.py

--- rssfeed/app.py
+++ rssfeed/app.py
@@ -10,7 +10,7 @@ from dotenv import load_dotenv
 load_dotenv()
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///rss.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = os.getenv("SECRET_KEY", "supersecretkey")
 db = SQLAlchemy(app)
