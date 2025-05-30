25e48f9c3653efc95b926c0ca95dfcc9949c079a
Matthew <74075600+mlafantaisie@users.noreply.github.com>
2025-05-30 12:23:04 -0400

    Update app.py

--- rssfeed/app.py
+++ rssfeed/app.py
@@ -87,9 +87,3 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for('login'))
-
-if __name__ == "__main__":
-    with app.app_context():
-        db.create_all()
-    fetch_feed()
-    app.run(debug=True)
