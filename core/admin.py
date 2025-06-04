5c5c225acc54f7bdc73f384015b77fe1450bbdc2
Matthew <74075600+mlafantaisie@users.noreply.github.com>
2025-06-04 12:44:49 -0400

    Delete core directory

--- core/admin.py
+++
@@ -1,4 +0,0 @@
-from django.contrib import admin
-from .models import DataEntry
-
-admin.site.register(DataEntry)
--- core/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class DataEntry(models.Model):
-    name = models.CharField(max_length=100)  # Example: Name of data entry
-    value = models.FloatField()  # Example: Numeric data value
-    created_at = models.DateTimeField(auto_now_add=True)  # Timestamp
-
-    def __str__(self):
-        return f"{self.name}: {self.value}"
