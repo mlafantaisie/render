8ea556d27fcd883c5fb6aed87a0d6a2b006db2c7
Matthew <74075600+mlafantaisie@users.noreply.github.com>
2025-06-04 12:47:39 -0400

    Update models.py

--- app/models.py
+++ app/models.py
@@ -1,1 +1,11 @@
+from sqlalchemy import Table, Column, Integer, String, MetaData
 
+metadata = MetaData()
+
+users = Table(
+    "users",
+    metadata,
+    Column("id", Integer, primary_key=True),
+    Column("username", String, unique=True, nullable=False),
+    Column("password", String, nullable=False),  # Store hashed passwords in prod
+)
