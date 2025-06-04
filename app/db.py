984075fe778f812c2c5b9e690b48f35e9344a0eb
Matthew <74075600+mlafantaisie@users.noreply.github.com>
2025-06-04 16:21:39 -0400

    Update db.py

--- app/db.py
+++ app/db.py
@@ -2,19 +2,13 @@ import databases
 import sqlalchemy
 import os
 
-DATABASE_URL = os.getenv("DATABASE_URL")
-
-if DATABASE_URL.startswith("postgresql+asyncpg://"):
-    DATABASE_URL = DATABASE_URL.replace("postgresql+asyncpg://", "postgresql://", 1)
-else
-    DATABASE_URL = "postgresql://" + DATABASE_URL
-
 database = databases.Database(DATABASE_URL)
 metadata = sqlalchemy.MetaData()
 
-print("Your database URL is: " + DATABASE_URL)
-
 engine = sqlalchemy.create_engine(
     DATABASE_URL,
+    connect_args={"sslmode": "require"},
     echo=True
 )
+
+print("Engine dialect in use:", engine.dialect.dbapi)
