563cb78ecba4cbd7bb02c12229c25dc81115d273
Matthew <74075600+mlafantaisie@users.noreply.github.com>
2025-06-04 14:43:12 -0400

    Update main.py

--- app/main.py
+++ app/main.py
@@ -6,7 +6,7 @@ from starlette.templating import Jinja2Templates
 
 from app.auth import router as auth_router
 from app.db import database, engine, metadata
-from app.models import users, auction_snapshots, snapshot_sessions
+from app import models
 from app.snapshots import take_snapshot
 
 app = FastAPI()
