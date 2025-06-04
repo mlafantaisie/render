2e03707d36082612f3bf39bcdf84a742eb37d89c
Matthew <74075600+mlafantaisie@users.noreply.github.com>
2025-06-04 16:34:18 -0400

    Update init_db.py

--- init_db.py
+++ init_db.py
@@ -1,10 +1,5 @@
-import os
-from sqlalchemy import create_engine
-from app.db import metadata
+from app.db import metadata, engine
 from app import models
-
-# Get database URL from env
-DATABASE_URL = os.getenv("DATABASE_URL")
 
 # Now models.py has loaded all tables into shared metadata
 metadata.create_all(engine)
