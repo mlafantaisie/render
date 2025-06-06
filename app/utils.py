9ea3a0ca613da9f4e810872465a0209c400c6c74
Matthew <74075600+mlafantaisie@users.noreply.github.com>
2025-06-06 13:14:14 -0400

    Update utils.py

--- app/utils.py
+++ app/utils.py
@@ -1,5 +1,12 @@
+from datetime import datetime
+
 def format_price(copper: int) -> str:
     gold = copper // 10000
     silver = (copper % 10000) // 100
     copper_remain = copper % 100
     return f"{gold}g {silver}s {copper_remain}c"
+
+def format_datetime(value: datetime):
+    if value is None:
+        return ""
+    return value.strftime("%Y-%m-%d %H:%M:%S")
